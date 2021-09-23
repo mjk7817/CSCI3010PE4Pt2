14adc569fc12adfa64ececd791ec06d2a554c9a8
mjk7817 <maigh.jammu@colorado.edu>
2021-09-23 13:51:07 -0600

    maighs updated displayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,10 +1,27 @@
 #include <iostream> 
 #include <vector>
 
-void CreateBoard()
+int CreateBoard()
 {
     int matrix [3][3] = {0};
-    std::cout<< matrix<<std::endl;
+    return matrix;
+}
+
+void DisplayBoard()
+{
+    system("cls");
+    std::cout<<"\n\n\t Tic Tac Toe\n\n";
+
+    std::cout<<"Player1 (X)- Player2 (Y)"<<std::endl;
+    std::cout<<std::endl;
+
+    std::cout<< "|    |   "<<std::endl;
+    std::cout<<" "<<matrix[1]<<" "<<matrix[2]<<" "<<matrix[3]<<std::endl;
+    std::cout<< "|    |   "<<std::endl;
+    std::cout<<" "<<matrix[4]<<" "<<matrix[5]<<" "<<matrix[6]<<std::endl;
+    std::cout<< "|    |   "<<std::endl;
+    std::cout<<" "<<matrix[7]<<" "<<matrix[8]<<" "<<matrix[9]<<std::endl;
+
 }
 
 int main()
