53173a884356cb4a1d64f4ae51669c098874ab3b
Brian <brno4758@colorado.edu>
2021-09-23 12:34:46 -0600

    DisplayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,7 +7,21 @@ void CreateBoard()
     std::cout<< matrix<<std::endl;
 }
 
+void DisplayBoard(int arr[][3])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
 int main()
 {
     CreateBoard();
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    DisplayBoard(arr);
 }
